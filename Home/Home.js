3752a58d0199ce6d81f25a3501d6d04599abc651
nada-arafa3 <152425052+nada-arafa3@users.noreply.github.com>
2025-07-09 22:07:08 +0300

    Update Home.js

--- Home/Home.js
+++ Home/Home.js
@@ -1,10 +1,6 @@
 function initHome() {
   let aboutElement = document.getElementById("home");
-  aboutElement.onclick = function change() {
-    aboutElement.style.fontSize = "50px";
-    aboutElement.style.backgroundColor = "yellow";
-    aboutElement.style.color = "red";
-  };
+ 
 }
 
 
