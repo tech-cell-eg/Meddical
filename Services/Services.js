fd535db6f9e2e81edfc8e7f48d662a9a99066bb0
Mohamed Mahmoud <125528675+Mohamedmahmmoud@users.noreply.github.com>
2025-07-10 16:49:15 +0300

    Update Services.js

--- Services/Services.js
+++ Services/Services.js
@@ -1,18 +1,32 @@
 function initServices() {
-  let aboutElement = document.getElementById("services");
- 
+  const cards = document.querySelectorAll(".card");
+  console.log(cards);
+  cards.forEach((card) => {
+    const image = card.querySelector(".image");
+    const icon = card.querySelector(".icon"); 
+
+    if (!image || !icon) return; 
+
+    image.addEventListener("mouseenter", () => {
+      icon.classList.add("d-none");
+    });
+
+    image.addEventListener("mouseleave", () => {
+      icon.classList.remove("d-none");
+    });
+  });
 }
 
 function renderServices() {
-  fetch("../Services/Services.html")
+  fetch("../Services/services.html")
     .then((response) => response.text())
     .then((html) => {
       root.innerHTML = html;
 
       const script = document.createElement("script");
-      script.src = "../Services/Services.js";
+      script.src = "../Services/services.js";
       script.onload = () => {
-        initAbout();
+        initServices();
       };
       document.body.appendChild(script);
     });
